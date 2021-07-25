979f38400ec470842ea6979ee62abd0f34784c3d
Zoltanbi <zoltanbiro001@gmail.com>
2021-07-24 17:47:14 -0700

    Installed node server and dependancies. Added gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <h1>CRUD APPLICATION</h1>
+
+      <div className="form">
+          <label>Movie Name:</label>
+          <input type="text" name="movieName" />
+          <label>Review:</label>
+          <input type="text" name="review" />
+
+          <button>Submit</button>
+      </div>
+
     </div>
   );
 }
