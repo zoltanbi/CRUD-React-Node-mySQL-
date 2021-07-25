2d4af92142aece24ba5371c5a433116d8451fe13
Zoltanbi <zoltanbiro001@gmail.com>
2021-07-25 12:20:19 -0700

    fixed CORS bug

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
@@ -10,15 +11,20 @@ const db = mysql.createPool({
     database: "crud_database"
 });
 
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}))
+
+// app.get('/api/get',)
 
 app.post('/api/insert', (req, res) => {
 
     const movieName = req.body.movieName;
     const movieReview = req.body.movieReview;
 
-    const sqlInsert = "INSERT INTO move_reviews (movie_name, review) VALUES (?,?)"
+    const sqlInsert = "INSERT INTO movie_reviews (movie_name, review) VALUES (?,?)"
     db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        console.log(err)
         console.log(result);
     });
 });
